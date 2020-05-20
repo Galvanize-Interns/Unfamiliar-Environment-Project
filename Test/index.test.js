327063b187ba878a9a0961fca8ae8feb9e790625
shauncarr22 <shauncarr2010@hotmail.com>
2020-05-20 12:41:45 -0700

    finsihed setting up first set of test

--- Test/index.test.js
+++ Test/index.test.js
@@ -1,14 +1,8 @@
-// Example of test using jest
-const sum = require('./example')
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
-});
-
-
 const tester = require('graphql-tester').tester
 
-describe('A query from graphql', () => {
+// MySql Test Cases
+// Query for all employee names
+describe("graphql employee query that grabs names", () => {
     const self = this;
     beforeAll(() => {
         self.test = tester({
@@ -16,27 +10,120 @@ describe('A query from graphql', () => {
             contentType: 'application/json'
         })
     })
-    it('Should recive books', done => {
+    it('Should grab the names of all empolyees', done => {
         self
-            .test(
-                JSON.stringify({
-                    query: 'query { books { name }}'
-                })
-            )
-            .then(res => {
-                let success = ""
-                for(let i = 0; i < res.data.books.length; i++){
-                    if(res.data.books[i].name === "Harry Potter and the Chamber of Secrets"){
-                        success = res.data.books[i].name
-                    }
-                }
-                expect(success).toBe("Harry Potter and the Chamber of Secrets")
-                done()
+        .test(
+            JSON.stringify({
+                query: 'query { employee { first_name last_name }}'
             })
-            .catch(err => {
-                expect(err).toBe(null)
-                done()
-            })
+        )
+        .then(res => {
+            let results = res.data.employee
+            expect(results).toBe([
+                {first_name:"Rodney", last_name:"Spears", start_date:"5/2020", city:'Austin', state:"Texas", position_id:1},
+                {first_name:"Shaun", last_name:"Carr", start_date:"5/2020", city:'Austin', state:"Texas", position_id:1},
+                {first_name:"Franky", last_name:"Leyva", start_date:"5/2020", city:'Austin', state:"Texas", position_id:1},
+                {first_name:"Poli", last_name:"Gonzalez", start_date:"5/2020", city:'Austin', state:"Texas", position_id:1},
+                {first_name:"Marco", last_name:"Ailiage", start_date:"5/2020", city:'Austin', state:"Texas", position_id:1}
+            ])
+            done()
+        })
+        .catch(err => {
+            expect(err).toBe(null)
+            done()
+        })
     })
 })
-
+// Query for first names and start dates for all employees 
+describe("graphql employee query that grabs first names and start dates", () => {
+    const self = this;
+    beforeAll(() => {
+        self.test = tester({
+            url: 'http://localhost:5000/graphql',
+            contentType: 'application/json'
+        })
+    })
+    it('Should grab the first names and their start dates of all employess', done => {
+        self
+        .test(
+            JSON.stringify({
+                query: 'query { employee { first_name start_date }}'
+            })
+        )
+        .then(res => {
+            let results = res.data.employee
+            expect(results).toBe([
+                {first_name:"Rodney",start_date:"5/2020"},
+                {first_name:"Shaun",start_date:"5/2020"},
+                {first_name:"Franky",start_date:"5/2020"},
+                {first_name:"Poli",start_date:"5/2020"},
+                {first_name:"Marco",start_date:"5/2020"}
+            ])
+            done()
+        })
+        .catch(err => {
+            expect(err).toBe(null)
+            done()
+        })
+    })
+})
+// Query for first first names and their position id
+describe("graphql employee query that grabs first names along with there postion ID", () => {
+    const self = this;
+    beforeAll(() => {
+        self.test = tester({
+            url: 'http://localhost:5000/graphql',
+            contentType: 'application/json'
+        })
+    })
+    it('Should grab the first name and position ids of all employees', done => {
+        self
+        .test(
+            JSON.stringify({
+                query: 'query { employee { first_name position_id }}'
+            })
+        )
+        .then(res => {
+            let results = res.data.employee
+            expect(results).toBe([
+                {first_name:"Rodney",position_id:1},
+                {first_name:"Shaun",position_id:1},
+                {first_name:"Franky",position_id:1},
+                {first_name:"Poli",position_id:1},
+                {first_name:"Marco",position_id:1}
+            ])
+            done()
+        })
+        .catch(err => {
+            expect(err).toBe(null)
+            done()
+        })
+    })
+})
+// Query for a single employee but return all there info.
+describe("graphql employee query that grabs all the info of a individual employee by name", () => {
+    const self = this;
+    beforeAll(() => {
+        self.test = tester({
+            url: 'http://localhost:5000/graphql',
+            contentType: 'application/json'
+        })
+    })
+    it('Should grab the names of all empolyees', done => {
+        self
+        .test(
+            JSON.stringify({
+                query: 'query { employee(name: "Poli") { first_name last_name start_date city state position_id }}'
+            })
+        )
+        .then(res => {
+            let results = res.data.employee
+            expect(results).toBe([{first_name:"Poli", last_name:"Gonzalez", start_date:"5/2020", city:'Austin', state:"Texas", position_id:1}])
+            done()
+        })
+        .catch(err => {
+            expect(err).toBe(null)
+            done()
+        })
+    })
+})
