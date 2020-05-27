774e9047833181b01f2c95304ca2ed0645d49e41
Poli Gonzalez <gonzalezpoli10@gmail.com>
2020-05-27 17:58:28 -0500

    patch test

--- db/migrations/20200520185155_initBooks.js
+++ db/migrations/20200520185155_initBooks.js
@@ -1,7 +1,22 @@
-exports.up = function(knex) {
-  
+exports.up = async function(knex, Promise) {
+  await knex.schema.createTable('users', table => {
+    table 
+        .increments('user_id')
+        .unsigned()
+        .primary();
+    table
+        .string('email')
+        .unique()
+        .notNullable();
+    table
+        .string('first_name')
+        .notNullable();
+    table
+        .string('last_name')
+        .notNullable();
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex, Promise) {
+  await knex.schema.dropTable("users");
 };
