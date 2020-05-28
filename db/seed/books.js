42b558bbf73162a00e7c9563d1a780ccd67ebbff
Rodney Spears <rodneyspears03@gmail.com>
2020-05-27 22:13:11 -0500

    rodneytest

---
+++ db/seed/books.js
@@ -0,0 +1,17 @@
+async function clear(knex) {
+	await knex('book').del()
+  };
+  async function seed(knex) {
+	await clear(knex)
+	await knex('book').insert({
+	  id: ID,
+	  title: 'Test action one',
+	  complete: false
+	});
+	await knex('book').insert({
+	  id:ID,
+	  title: 'Test action two',
+	  complete: false
+	});
+  };
+  module.exports = { clear, seed }
